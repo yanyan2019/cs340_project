95328cacc25d0292e8ba395e0800376e2bc79e94
YanYan Lee <leeya@oregonstate.edu>
2019-12-02 00:54:33 -0800

    merge initial codes with provided sample code

---
+++ group_project/src/config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+  host: 'classmysql.engr.oregonstate.edu',
+  user: 'cs340_leeya',
+  password: '7750',
+  database: 'cs340_leeya'
+};
---
+++ group_project/src/server.js
@@ -0,0 +1,72 @@
+/*server.js with express*/
+
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+/* configure handlebars*/
+const hbs = exphbs.create({
+	defaultLayout: 'main', 
+	extname: '.hbs'
+});
+
+
+/*add layouts */
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(path.basename(__dirname), 'views'));
+
+
+/* set up static content serving*/
+app.use(express.static(path.join(path.basename(__dirname),'public')));
+
+
+/* Create a database connection. This is our middleware function that will initilaze
+ * a new connection to our MYSQL database on every request.
+ */
+const config = require('./config');
+function connectDb(req, res, next){
+	console.log('Connection to the database');
+	let connection = mysql.createConnection(config);
+	connection.connect();
+	req.db = connection;
+	console.log('Database conncected');
+	next();
+}
+
+/* Handler for main page. The middleware pipeline includes our custom 'connectDb()' function
+ * that creates our database connection and exposes it as 'req.db'.
+ */
+app.get('/',connectDb, function(req, res){
+	res.render('home');
+	close(req);
+});
+
+/* not found 404*/
+app.get('*', function(req,res){
+	res.status(400).render('notfound');
+	close (req);
+});
+
+/* Handled all of the resources we need to clean up, In this case, 
+ * we just need to close the database connection.
+ */
+function close(req) {
+	if (req.db){
+		req.db.end();
+		req.db = undefined;
+		console.log('Database connection closed');
+	}
+}
+
+
+app.listen(port, function(err){
+	if(err){
+		throw err;
+	}
+	console.log("== Server listening on port", port);
+});
